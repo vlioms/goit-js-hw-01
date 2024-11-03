f4e90f7d44e8b1145ba8892603b57d13a46ceec7
vlioms <villiomss14@gmail.com>
2024-11-03 12:24:17 +0100

    all

--- js/task-1.js
+++ js/task-1.js
@@ -3,4 +3,6 @@ function makeTransaction(quantity, pricePerDroid) {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
+console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500)); 
